d4b61d235a49f6eda03eb99055744719305ad6b1
alvinwidi33 <alvin.widi@uiac.id>
2024-07-16 16:43:06 +0700

    benerin sedikit

--- tugas2-nodejs-intro/nomor_tiga_export.js
+++
@@ -1,4 +0,0 @@
-const tambah = require("./nomor_tiga");
-
-const hasil = tambah(5, 3);
-console.log(`Hasil penjumlahannya adalah: ${hasil}`);
--- tugas2-nodejs-intro/tambah.js
+++ tugas2-nodejs-intro/tambah.js
@@ -1,6 +1,7 @@
 // No. 3 Buat modul Node.js yang mengekspor fungsi untuk menjumlahkan dua angka,
 // dan impor modul tersebut di file lain.
-const tambah = require("./tambah");
 
-const hasil = tambah(5, 3);
-console.log(`Hasil penjumlahannya adalah: ${hasil}`);
+function tambah(x, y){
+    return x + y;
+}
+module.exports = tambah;
